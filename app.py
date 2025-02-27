5115d5f7b217a262446d673814112d5f4a05cf5e
samvela <samuelverso2708@gmail.com>
2025-02-27 00:34:16 -0600

    Primer comentario de mi rama

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 #comentario de prueba
 app = Flask(__name__)
-
+##Esto es una linea para decirte que eres bien homosexual
 @app.route('/')
 def hello_world():
     return 'Hola Mundo'
